322c3f7de56b39b389c692c9a9f7227b32425bc4
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-04-04 15:08:44 +0200

    Semi working navbar

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,22 @@
-const DashboardPage = () => {
+import React from "react";
+import prismadb from "@/lib/prismadb";
+
+
+interface DashboardPageProps {
+    params: { storeId: string }
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    })
+
   return(
       <div>
-          Dashm
+          Current Store: {store?.name}
       </div>
   )
 }
--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {auth} from "@clerk/nextjs";
 import {redirect} from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import Navbar from "@/components/navbar";
 
 export default async function DashboardLayout({
     children,
@@ -25,9 +26,7 @@ export default async function DashboardLayout({
     }
     return(
         <>
-            <div>
-                This will be a Navbar
-            </div>
+            <Navbar/>
             {children}
         </>
     )
