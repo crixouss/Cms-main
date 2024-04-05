e49ab1fce5554eec89a094ddd68e902d53a43ca4
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-04-05 13:37:51 +0200

    fixed the false stated path error (404)

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -41,7 +41,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     const onSubmit = async (data: SettingsFormValues) => {
         try {
             setLoading(true)
-            await axios.patch(`/api/stores${params.storeId}`, data)
+            await axios.patch(`/api/stores/${params.storeId}`, data)
             router.refresh()
             toast.success("Store updated successfully.")
         } catch (error) {
