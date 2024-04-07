5b48db35e27ad129d47924c2b38a5a64f81205d8
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-04-07 22:00:04 +0200

    fiexd nav billboard not glowing

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,15 +5,23 @@ import {Button} from "@/components/ui/button";
 import {Plus} from "lucide-react";
 import {Separator} from "@/components/ui/separator";
 import {useParams, useRouter} from "next/navigation";
+import {Billboard} from "@prisma/client";
+import React from "react";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+    data: Billboard[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+    data
+}) => {
     const router = useRouter();
     const params = useParams();
 
     return(
         <>
             <div className={"flex items-center justify-between"}>
-                <Heading title={"Billboards (0)"} description={"Manage billboards for your store"}/>
+                <Heading title={`Billboards (${data.length})`} description={"Manage billboards for your store"}/>
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className={"mr-2 h-4 w-4"}/>
                     Add New
