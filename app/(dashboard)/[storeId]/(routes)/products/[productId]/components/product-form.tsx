8012c027da5b6e61a342db46391afad20a2d7323
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-04-17 16:03:14 +0200

    Fixed the bug with my db and also fixed a bug where checkbox doesnt work

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -236,7 +236,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                         <FormField control={form.control} name={"isFeattured"} render={({field}) => (
                             <FormItem className={"flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4"}>
                                 <FormControl>
-                                    <Checkbox checked={field.value} onChange={field.onChange}/>
+                                    <Checkbox checked={field.value} onCheckedChange={field.onChange}/>
                                 </FormControl>
                                 <div className={"space-y-1 leading-none"}>
                                     <FormLabel>
@@ -251,7 +251,7 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                         <FormField control={form.control} name={"isArchived"} render={({field}) => (
                             <FormItem className={"flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4"}>
                                 <FormControl>
-                                    <Checkbox checked={field.value} onChange={field.onChange}/>
+                                    <Checkbox checked={field.value} onCheckedChange={field.onChange}/>
                                 </FormControl>
                                 <div className={"space-y-1 leading-none"}>
                                     <FormLabel>
