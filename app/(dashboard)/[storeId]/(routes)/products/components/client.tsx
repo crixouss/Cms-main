2a64b137344fa5bab5e3975a3c6d4f90c26f4150
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-04-17 12:10:59 +0200

    Updating db

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import {Heading} from "@/components/ui/heading";
+import {Button} from "@/components/ui/button";
+import {Database, Plus} from "lucide-react";
+import {Separator} from "@/components/ui/separator";
+import {useParams, useRouter} from "next/navigation";
+import {Billboard} from "@prisma/client";
+import React from "react";
+import {BillboardColumn, columns} from "@/app/(dashboard)/[storeId]/(routes)/billboards/components/columns";
+import {DataTable} from "@/components/ui/data-table";
+import {ApiList} from "@/components/ui/api-list";
+
+interface BillboardClientProps {
+    data: BillboardColumn[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+
+    return(
+        <>
+            <div className={"flex items-center justify-between"}>
+                <Heading title={`Billboards (${data.length})`} description={"Manage billboards for your store"}/>
+                <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+                    <Plus className={"mr-2 h-4 w-4"}/>
+                    Add New
+                </Button>
+            </div>
+            <Separator/>
+            <DataTable searchKey={"label"} columns={columns} data={data}/>
+            <Heading title={"API"} description={"Manage API calls for Billboards"}/>
+            <Separator/>
+            <ApiList entityName={"billboards"} entityIdName={"billboardId"}/>
+        </>
+    )
+}
