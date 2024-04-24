5887d3e4b7333c7731e5fdbd61be11b066d94a75
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-04-24 12:10:16 +0200

    working graph using recharts library

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -7,6 +7,8 @@ import {formatter} from "@/lib/utils";
 import {getTotalRevenue} from "@/actions/get-total-revenue";
 import {getSalesCount} from "@/actions/get-sales-count";
 import {getStockCount} from "@/actions/get-stock-count";
+import {Overview} from "@/components/overview";
+import {getGraphRevenue} from "@/actions/get-graph-revenue";
 
 
 interface DashboardPageProps {
@@ -17,14 +19,15 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
     const totalRevenue = await getTotalRevenue(params.storeId)
     const salesCount = await getSalesCount(params.storeId)
     const stockCount = await getStockCount(params.storeId)
+    const graphRevenue = await getGraphRevenue(params.storeId)
 
   return(
       <div className={"flex-col"}>
           <div className={"flex-1 space-y-4 p-8 pt-6"}>
               <Heading title={"Dashboard"} description={"Overview of your store"} />
               <Separator/>
-              <div className={"grid grid-4 grid-cols-3"}>
-                  <Card className={"p-4"}>
+              <div className={"grid grid-4 grid-cols-3 "}>
+                  <Card className={""}>
                       <CardHeader className={"flex flex-row items-center justify-between space-y-0 pb-2"}>
                           <CardTitle className={"text-sm font-medium"}>
                               Total Revenue
@@ -37,7 +40,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
                           </div>
                       </CardContent>
                   </Card>
-                  <Card className={"p-4"}>
+                  <Card className={""}>
                       <CardHeader className={"flex flex-row items-center justify-between space-y-0 pb-2"}>
                           <CardTitle className={"text-sm font-medium"}>
                               Sales
@@ -50,7 +53,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
                           </div>
                       </CardContent>
                   </Card>
-                  <Card className={"p-4"}>
+                  <Card className={""}>
                       <CardHeader className={"flex flex-row items-center justify-between space-y-0 pb-2"}>
                           <CardTitle className={"text-sm font-medium"}>
                               Products in Stock
@@ -64,6 +67,16 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
                       </CardContent>
                   </Card>
               </div>
+              <Card className={"col-span-4"}>
+                  <CardHeader>
+                      <CardTitle>
+                          Overview
+                      </CardTitle>
+                  </CardHeader>
+                  <CardContent className={"pl-2"}>
+                      <Overview data={graphRevenue}/>
+                  </CardContent>
+              </Card>
           </div>
       </div>
   )
---
+++ components/overview.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import React from "react";
+import {Bar, BarChart, ResponsiveContainer,  XAxis, YAxis} from "recharts";
+
+interface OverviewProps {
+    data: any[];
+}
+
+export const Overview: React.FC<OverviewProps> = ({data
+}) => {
+    return (
+        <ResponsiveContainer width="99%" height={300}>
+            <BarChart  data={data}>
+                <XAxis dataKey="name"
+                       stroke={"#888888"}
+                       fontSize={12}
+                       tickLine={false}
+                       axisLine={false}/>
+                <YAxis tickFormatter={(value) => `$${value}`}
+                       stroke={"#888888"}
+                       fontSize={12}
+                       tickLine={false}
+                       axisLine={false}/>
+                <Bar dataKey={"total"} fill={"#3498db"} radius={[4, 4, 0, 0]}/>
+            </BarChart>
+        </ResponsiveContainer>
+    )
+}
