634c0c664050c54c385709be5f7bcd2b700b2019
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-10-03 20:21:29 +0200

    WORKS!!!

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -11,6 +11,7 @@ export default async function DashboardLayout({
     children: React.ReactNode;
     params: { storeId: string}
 }){
+
     const { userId } = auth();
     if (!userId) {
         redirect('/sign-in');
