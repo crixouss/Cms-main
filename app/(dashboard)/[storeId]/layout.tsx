9d3f402be29d1b42996350435fd930d1622e9628
Dzheysan Apturahim <apturahimjason@gmail.com>
2024-10-11 22:04:50 +0200

    WORKS!!!

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -8,8 +8,10 @@ export default async function DashboardLayout({
     children,
     params
 }: {
+
     children: React.ReactNode;
     params: { storeId: string }
+
 }){
 
     const { userId } = auth();
